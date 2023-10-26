37d98db8fe1a2f8cde4b9aeeaee988605e957236
Donnie1129 <drn112995@gmail.com>
2023-10-26 18:05:48 -0500

    updated questions for JS

--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,45 @@
 var questions = [
     {
-      question: "What color is the sky?",
+      question: "What is Javascript used for??",
       answers: [
-        { Text: "Blue", correct: true },
-        { Text: "Pink", correct: false },
-        { Text: "Orange", correct: false },
-        { Text: "Green", correct: false }
+        { text: "To add style to a webpage", correct: false },
+        { text: "To search for information about coding", correct: false },
+        { text: "To add functionality to a webpage", correct: true },
+        { text: "Nothing, it's useless", correct: false }
       ]
     },
     {
-      question: "What color is the grass?",
+      question: "How do we link Javascript to the HTML?",
       answers: [
-        { Text: "Red", correct: false },
-        { Text: "Green", correct: true },
-        { Text: "Purple", correct: false },
-        { Text: "Black", correct: false }
+        { text: "<script>", correct: true },
+        { text: "<href>", correct: false },
+        { text: "<link>", correct: false },
+        { text: "<id>", correct: false }
       ]
     },
     {
-      question: "This is Question 3",
+      question: "Which character(s) goes after a function is called?",
       answers: [
-        { Text: "text for question", correct: false },
-        { Text: "text for question", correct: false },
-        { Text: "text for question", correct: true },
-        { Text: "text for question", correct: false }
+        { text: "=", correct: false },
+        { text: "->", correct: false },
+        { text: "[]", correct: false },
+        { text: "()", correct: true }
       ]
     },
+    // to whomever grades this, this is an inside joke with our instructor
     {
-      question: "This is Question 4",
+      question: "According to Derek Drummond(our instructor) which of the following is typed correctly?",
       answers: [
-        { Text: "text for question", correct: false },
-        { Text: "text for question", correct: false },
-        { Text: "text for question", correct: false },
-        { Text: "text for question", correct: true }
+        { text: "i+i", correct: false },
+        { text: "+i+", correct: false },
+        { text: "i++", correct: false },
+        { text: "++i", correct: true }
+
       ]
     }
   ];
   
+  var answerResponseElement = document.getElementById('answer-response');
   var startButton = document.getElementById('btn');
   var questionElement = document.getElementById('question');
   var answerButtonsContainer = document.getElementById('answer-buttons');
@@ -44,46 +47,42 @@ var questions = [
   var timerInterval;
   
   function displayQuestion() {
-    if (currentQuestionIndex < questions.length) {
-      var currentQuestion = questions[currentQuestionIndex];
-      questionElement.innerHTML = currentQuestion.question;
-      questionElement.style.display = "block";
-      answerButtonsContainer.style.display = "block";
-      startButton.style.display = "none"; // Hide the start button
-      Array.from(answerButtonsContainer.children).forEach((button, index) => {
-        if (currentQuestion.answers[index]) {
-          button.textContent = currentQuestion.answers[index].Text;
-          button.disabled = false; // Re-enable the answer buttons
-        }
-      });
+    var currentQuestion = questions[currentQuestionIndex];
+    questionElement.innerHTML = currentQuestion.question;
+    questionElement.style.display = "block";
+    answerButtonsContainer.style.display = "block";
+    startButton.style.display = "none";
   
-      // Hide the Next button
-      var nextButton = document.getElementById('next-btn');
-      nextButton.style.display = 'none';
-    }
+    Array.from(answerButtonsContainer.children).forEach((button, index) => {
+      var answer = currentQuestion.answers[index];
+      if (answer) {
+        button.textContent = answer.text;
+        button.disabled = false;
+      }
+    });
+  
+    var nextButton = document.getElementById('next-btn');
+    nextButton.style.display = 'none';
   }
   
   function handleAnswerButtonClick(index) {
     var currentQuestion = questions[currentQuestionIndex];
-    var answerResponseElement = document.getElementById('answer-response');
-    answerResponseElement.textContent = "none";
-  
     if (currentQuestion.answers[index].correct) {
+      answerResponseElement.textContent = "block";
       answerResponseElement.textContent = 'Correct!';
       answerResponseElement.classList.add('correct');
       answerResponseElement.classList.remove('incorrect');
     } else {
+      answerResponseElement.textContent = "block";
       answerResponseElement.textContent = 'Incorrect!';
       answerResponseElement.classList.add('incorrect');
       answerResponseElement.classList.remove('correct');
-      timeLeft -= 10;
+      timeLeft -= 15;
     }
   
-    // Show the Next button after answering
     var nextButton = document.getElementById('next-btn');
     nextButton.style.display = 'block';
   
-    // Disable the answer buttons until the next question is displayed
     Array.from(answerButtonsContainer.children).forEach((button) => {
       button.disabled = true;
     });
@@ -102,17 +101,13 @@ var questions = [
     clearInterval(timerInterval);
     document.getElementById('timer').textContent = timeLeft;
   
-    // Calculate the score based on the time left
     var score = timeLeft;
   
-    // Display the score
     questionElement.textContent = "Quiz Over! Your Score: " + score;
     answerButtonsContainer.style.display = "none";
   
-    // Prompt the user for their initials
     var initials = window.prompt("Enter your initials:");
   
-    // Save the initials and score to local storage
     var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
     highScores.push({ initials: initials, score: score });
     localStorage.setItem("highScores", JSON.stringify(highScores));
@@ -123,33 +118,24 @@ var questions = [
   }
   
   function startQuiz() {
-    // Reset the quiz state
     currentQuestionIndex = 0;
     timeLeft = 60;
   
-    // Reset the timer display
     document.getElementById('timer').textContent = timeLeft;
-  
-    // Reset the answer response element
     document.getElementById('answer-response').textContent = '';
   
-    // Reset the question and answer buttons
     displayQuestion();
   
-    // Hide the next button
     document.getElementById('next-btn').style.display = 'none';
   
-    // Enable the answer buttons
     Array.from(answerButtonsContainer.children).forEach((button) => {
       button.disabled = false;
     });
   
-    // Start the timer
     timerInterval = setInterval(function() {
       timeLeft--;
       document.getElementById('timer').textContent = timeLeft;
   
-      // Check if time is up
       if (timeLeft <= 0) {
         clearInterval(timerInterval);
         endQuiz();
@@ -167,34 +153,36 @@ var questions = [
     var answerIndex = Array.from(answerButtonsContainer.children).indexOf(answerButton);
     handleAnswerButtonClick(answerIndex);
   });
-  
-  // Add event listener to the Next button
+
   var nextButton = document.getElementById('next-btn');
-  nextButton.addEventListener('click', displayNextQuestion);
+nextButton.addEventListener('click', displayNextQuestion);
+
+var highScoresButton = document.getElementById('high-scores-button');
+var highScoresList = document.getElementById('high-scores-list');
+var isHighScoresVisible = false;
+
+highScoresButton.addEventListener('click', function() {
+  var highScores = JSON.parse(localStorage.getItem('highScores')) || [];
   
-  var highScoresButton = document.getElementById('high-scores-button');
-  var highScoresList = document.getElementById('high-scores-list');
-  
-  highScoresButton.addEventListener('click', function() {
-    // Retrieve high scores from local storage
-    var highScores = JSON.parse(localStorage.getItem('highScores')) || [];
-  
-    // Clear the high scores list
+  if (!isHighScoresVisible) {
     highScoresList.innerHTML = '';
-  
-    // Display each high score in the list
+    
     highScores.forEach(function(score) {
       var listItem = document.createElement('li');
       listItem.textContent = score.initials + ': ' + score.score;
       highScoresList.appendChild(listItem);
     });
-  });
-  var clearButton = document.getElementById('clear-button');
-  
-  clearButton.addEventListener('click', function() {
-    // Remove high scores from local storage
-    localStorage.removeItem('highScores');
-  
-    // Clear the high scores list in the DOM
+    
+    isHighScoresVisible = true;
+  } else {
     highScoresList.innerHTML = '';
-  });
+    isHighScoresVisible = false;
+  }
+});
+
+var clearButton = document.getElementById('clear-button');
+
+clearButton.addEventListener('click', function() {
+  localStorage.removeItem('highScores');
+  highScoresList.innerHTML = '';
+});
